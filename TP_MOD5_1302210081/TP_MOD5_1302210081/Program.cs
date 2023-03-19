356d3444547c1cbd14fd7bca98bdf7851e408590
hilmifauziyyah <hilmifs2113@gmail.com>
2023-03-19 23:51:54 +0700

    menambahkan class HaloGeneric dengan method  SapaUser

--- TP_MOD5_1302210081/TP_MOD5_1302210081/Program.cs
+++ TP_MOD5_1302210081/TP_MOD5_1302210081/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public class HaloGeneric
+    {
+        public static void sapaUser<H>(H inputuser)
+        {
+            Console.WriteLine("Hallo user " + inputuser);
+        }
+    }
+    public class DataGeneric<H>
+    {
+        private H data;
+        public DataGeneric(H data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data Yang Tersimpan Adalah: " + data);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        HaloGeneric.sapaUser<string>("Hilmi Fauziyyah");
+        DataGeneric<string> nim = new DataGeneric<string>("1302210081");
+        nim.PrintData();
+    }
+}
